67720c8185c6d9dc1997be05a16d7d4748105377
chrisesbueno <alvarezchristopherve@gmail.com>
2022-10-06 15:11:16 -0400

    Proyecto Checkout Page: Commit 01

---
+++ README.md
@@ -0,0 +1,60 @@
+<!-- Please update value in the {}  -->
+
+<h1 align="center">Checkout Page</h1>
+
+<div align="center">
+   Solution for a challenge from  <a href="http://devchallenges.io" target="_blank">Devchallenges.io</a>.
+</div>
+
+<div align="center">
+  <h3>
+    <a href="https://{your-demo-link.your-domain}">
+      Demo
+    </a>
+    <span> | </span>
+    <a href="https://{your-url-to-the-solution}">
+      Solution
+    </a>
+    <span> | </span>
+    <a href="https://devchallenges.io/challenges/0J1NxxGhOUYVqihwegfO">
+      Challenge
+    </a>
+  </h3>
+</div>
+
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [Overview](#overview)
+- [Features](#features)
+- [Contact](#contact)
+
+<!-- OVERVIEW -->
+
+## Overview
+![screenshot](https://user-images.githubusercontent.com/16707738/92399059-5716eb00-f132-11ea-8b14-bcacdc8ec97b.png)
+
+![screenshot](https://user-images.githubusercontent.com/16707738/92399059-5716eb00-f132-11ea-8b14-bcacdc8ec97b.png)
+
+![screenshot](https://user-images.githubusercontent.com/16707738/92399059-5716eb00-f132-11ea-8b14-bcacdc8ec97b.png)
+
+![screenshot](https://user-images.githubusercontent.com/16707738/92399059-5716eb00-f132-11ea-8b14-bcacdc8ec97b.png)
+
+- Where can I see your demo?
+  <h3><a href="https://{your-demo-link.your-domain}">Demo</a></h3>
+- What was your experience?
+  <h3>It was quite stressful because I wanted to add features with js and I had to learn how to</h3>
+- What have you learned/improved?
+  <h3>How to use the DOM and a lot of new attributes and methods that I didn't know about</h3>
+
+## Features
+
+<!-- List the features of your application or follow the template. Don't share the figma file here :) -->
+
+This application/site was created as a submission to a [DevChallenges](https://devchallenges.io/challenges) challenge. The [challenge](https://devchallenges.io/challenges/0J1NxxGhOUYVqihwegfO) was to build an application to complete the given user stories.
+
+## Contact
+
+- GitHub [@chrisesbueno](https://github.com/chrisesbueno)
+- Twitter [@chrisesbueno](https://twitter.com/chrisesbueno)
---
+++ main.js
@@ -0,0 +1,113 @@
+const item = document.getElementsByClassName('itemInfo');
+const arrayItem = [...item];
+arrayItem.forEach(element => {
+    /* VARIABLES Y CODIGOS PARA SUMAR Y RESTAR CANTIDAD DE ITEMS -START- */
+    let value = 0;
+    const setItem = element.children[1];
+    const addItem = setItem.children[2];
+    const restItem = setItem.children[0];
+    addItem.addEventListener('click', function add() {
+        value++;
+        setValue(value);
+        setTotal(true);
+    });
+    restItem.addEventListener('click', function rest() {
+        if (value === 0) return;
+        value--;
+        setValue(value);
+        setTotal(false);
+    });
+    function setValue(val) {
+        valueItem = setItem.children[1];
+        valueItem.innerHTML = val;
+    }
+    /* VARIABLES Y CODIGOS PARA SUMAR Y RESTAR CANTIDAD DE ITEMS -END- */
+    function setTotal(e) {
+        let shipping = 9.50; // esto es el precio por cada item de shipping
+        let totalShipping = Number((document.getElementById('shipping').innerHTML).slice(1)); // esto es el precio del shipping
+        let price = Number((element.children[0].children[1].children[0].innerHTML).slice(1)); // esto es el precio del item
+        let totalPrice = Number((document.getElementById('totalPrice').innerHTML).slice(1)); // esto es el precio del total de items
+        // console.log(price, totalPrice, shipping, totalShipping)
+        if (e) {
+            document.getElementById('shipping').innerHTML = '$' + String((totalShipping + shipping).toFixed(2));
+            document.getElementById('totalPrice').innerHTML = '$' + String((totalPrice + price).toFixed(2));
+        } else {
+            document.getElementById('shipping').innerHTML = '$' + String((totalShipping - shipping).toFixed(2));
+            document.getElementById('totalPrice').innerHTML = '$' + String((totalPrice - price).toFixed(2));
+        }
+    }
+});
+
+// console.log(document.getElementsByClassName('itemInfo'))
+
+/* ESTO ES EL API PARA SELECCIONAR PAISES NO SE SI SEA BUENA PRACTICA HACERLO AQUI */
+function createOption(element) {
+    return document.createElement(element);
+}
+
+function append(parent, element) {
+  return parent.appendChild(element);
+}
+
+const select = document.getElementById('formCountry');
+const url = 'https://restcountries.com/v2/all?fields=name';
+
+fetch(url)
+    .then(response => response.json())
+    .then(function(data) {
+        return data.map(function(country) {
+            let option = createOption('option');
+            option.innerHTML = country.name
+            append(select, option);
+        })
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+
+/* ESTO ES EL API PARA SELECCIONAR PAISES NO SE SI SEA BUENA PRACTICA HACERLO AQUI */
+
+/* SUBMIT CON ALERT */
+
+function setSubmit() {
+    const email = document.getElementById('formEmail').value;
+    const phone = document.getElementById('formPhone').value;
+    const fullname = document.getElementById('formName').value;
+    const city = document.getElementById('formCity').value;
+    const country = document.getElementById('formCountry').value;
+    const postal = document.getElementById('formPostal').value;
+    const shipping = Number((document.getElementById('shipping').innerHTML).slice(1));
+    const total = Number((document.getElementById('totalPrice').innerHTML).slice(1));
+    const check = document.getElementById('check');
+    
+    let value = false;
+    if (email != '' && email != '' && phone != '' && fullname != '' && city != '' && country != '' && postal != '') {
+        value = true
+    }
+    if (value == true && shipping !=0 && total != 0) {  
+        confirm(`
+        || Your information was sent successfully ||
+        Your E-mail: ${email}
+        Your phone: ${phone}
+        Your fullname: ${fullname}
+        Your city: ${city}
+        Your country: ${country}
+        Your postal: ${postal}
+        Shipping Price: $${shipping}
+        Total Price: $${total}
+        `);
+        if (check.checked === true) {
+            alert('Your information was saved correctly')
+        } else {
+            reset()
+        }
+    } else {
+        alert("Please enter your info and items");
+        return false;
+    } 
+    
+    function reset() {
+        document.querySelector('form').action = '/'
+    }
+}
+
